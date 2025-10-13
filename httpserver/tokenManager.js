55517e3becfa2801a4a9b5c944ba7ee9f248ab26
Pois0nBread <pois0nbreads@gmail.com>
2025-10-13 17:05:38 +0800

    新建Token管理器，内部细节未实现

--- httpserver/tokenManager.js
+++ httpserver/tokenManager.js
@@ -3,3 +3,29 @@
  * 
  * 
  */
+class TokenManager {
+    constructor(userDAO, roleDAO) {
+        this.userDAO = userDAO;
+        this.roleDAO = roleDAO;
+    }
+
+    /**
+     * 从数据库同步用户以及权限
+     */
+    async syncDatabase() {
+
+    }
+
+    getInfoByToken(token) {
+
+    }
+
+    createToken(user) {
+
+    }
+
+    destoryToken(token) {
+
+    }
+}
+module.exports = TokenManager;
